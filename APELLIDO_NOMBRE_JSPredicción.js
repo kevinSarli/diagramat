dbc875939b26a03f7d5c2235c97a0c0bd00c3a0b
kevinSarli <kevin.sarli99@gmail.com>
2022-02-28 13:29:15 -0300

    console prediccion

---
+++ APELLIDO_NOMBRE_JSPredicción.js
@@ -0,0 +1,15 @@
+function myBirthYearFunc(){
+    console.log("Nací en" + 1980);//naci en 1980
+}
+
+function myBirthYearFunc(birthYearInput){
+    console.log("Nací en" + birthYearInput);//naci en 1980
+}
+
+function add(num1, num2){
+    console.log("¡Sumando números!");//sumando numeros
+    console.log("num1 es: " + num1); //num1 es 10
+    console.log("num2 es: " + num2);//num2 es 20
+    var sum = num1 + num2;
+    console.log(sum);//30
+}
